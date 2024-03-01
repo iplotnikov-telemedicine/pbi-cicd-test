90e690afaa7d3323939e1260dad709a2ddc68924
Igor <plotnikov.sseu@gmail.com>
2024-03-01 12:35:29 -0500

    Solution 1, remove .git

--- commit.sh
+++ commit.sh
@@ -12,7 +12,7 @@ rm -rf Brain-Squeezes/.git
 git fetch --unshallow
 
 # Pull changes from Azure DevOps if its exiting branch and have commits on it
-git pull https://$AZUSERNAME:$AZUREPAT@dev.azure.com/$AZORG/PowerBI/_git/pbi-cicd-test.git
+git pull https://$AZUSERNAME:$AZUREPAT@dev.azure.com/$AZORG/PowerBI/_git/pbi-cicd-test
 
 #git checkout -b $github_to_azure_sync
 
@@ -23,4 +23,4 @@ git config --global user.name "$AZUSERNAME"
 # Add all changes into stage, commit, and push to Azure DevOps
 git add .
 git commit -m "Sync from GitHub to Azure DevOps"
-git push --force https://$AZUSERNAME:$AZUREPAT@dev.azure.com/$AZORG/PowerBI/_git/pbi-cicd-test.git
+git push --force https://$AZUSERNAME:$AZUREPAT@dev.azure.com/$AZORG/PowerBI/_git/pbi-cicd-test
