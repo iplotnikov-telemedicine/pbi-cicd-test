1cf5d19f79e52f5b6f03ceff04491ed1e4e2758c
Igor <plotnikov.sseu@gmail.com>
2024-03-02 07:52:06 -0500

    Add project_name as repo secret

--- commit.sh
+++ commit.sh
@@ -4,6 +4,7 @@ AZUREPAT=$AZUREPAT
 AZUSERNAME=$AZUSERNAME
 AZUSER_EMAIL=$AZUSER_EMAIL
 AZORG=$AZORG
+AZPROJECT=$AZPROJECT
 
 # Remove Git information (for fresh git start)
 rm -rf Brain-Squeezes/.git
@@ -12,9 +13,7 @@ rm -rf Brain-Squeezes/.git
 git fetch --unshallow
 
 # Pull changes from Azure DevOps if its exiting branch and have commits on it
-git pull https://$AZUSERNAME:$AZUREPAT@dev.azure.com/$AZORG/PowerBI/_git/pbi-cicd-test
-
-#git checkout -b $github_to_azure_sync
+git pull https://$AZUSERNAME:$AZUREPAT@dev.azure.com/$AZORG/$AZPROJECT/_git/pbi-cicd-test
 
 # Set Git user identity
 git config --global user.email "$AZUSER_EMAIL"
@@ -23,4 +22,4 @@ git config --global user.name "$AZUSERNAME"
 # Add all changes into stage, commit, and push to Azure DevOps
 git add .
 git commit -m "Sync from GitHub to Azure DevOps"
-git push --force https://$AZUSERNAME:$AZUREPAT@dev.azure.com/$AZORG/PowerBI/_git/pbi-cicd-test
+git push --force https://$AZUSERNAME:$AZUREPAT@dev.azure.com/$AZORG/$AZPROJECT/_git/pbi-cicd-test
