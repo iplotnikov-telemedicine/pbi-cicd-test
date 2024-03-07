0950a8f09abc6641f160a148b66d42c52786b0b1
Igor <plotnikov.sseu@gmail.com>
2024-03-07 13:58:09 -0500

    Token request to send with client_secret

--- update_workspace.sh
+++ update_workspace.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
 # Get access token
-token_response=$(curl --location 'https://login.windows.net/common/oauth2/token' \
+token_response=$(curl --location 'https://login.microsoftonline.com/'"$TENANT_ID"'/oauth2/v2.0/token' \
     --header 'Content-Type: application/x-www-form-urlencoded' \
-    --header 'Cookie: fpc=AqaCGRSl6mZAgM-xxtlFBrbzHxbfAQAAADqaet0OAAAAYDQJkgEAAABEmnrdDgAAAA; stsservicecookie=estsfd; x-ms-gateway-slice=estsfd' \
     --data-urlencode 'client_id='"$CLIENT_ID"'' \
-    --data-urlencode 'grant_type=password' \
-    --data-urlencode 'resource=https://analysis.windows.net/powerbi/api' \
-    --data-urlencode 'username='"$AZUSER_EMAIL"'' \
-    --data-urlencode 'password='"$AZUSERPASSWORD"'' \
-    --data-urlencode 'tenant_id='"$TENANT_ID"'')
+    --data-urlencode 'scope=https://api.fabric.microsoft.com/.default' \
+    --data-urlencode 'client_secret='"$CLIENT_SECRET"'' \
+    --data-urlencode 'grant_type=client_credentials')
 
 access_token=$(echo "$token_response" | grep -o '"access_token":"[^"]*' | awk -F'"' '{print $4}')
 
@@ -21,12 +18,6 @@ status_response=$(curl -s -X GET \
 # Extract parameters using Bash string manipulation
 workspaceHead=$(echo "$status_response" | grep -o '"workspaceHead":"[^"]*' | sed 's/"workspaceHead":"//')
 remoteCommitHash=$(echo "$status_response" | grep -o '"remoteCommitHash":"[^"]*' | sed 's/"remoteCommitHash":"//')
-
-
-echo "<workspaceHead is $workspaceHead>"
-echo "<remoteCommitHash is $remoteCommitHash>"
-echo "<access_token is $access_token>"
-
 
 # Update the workspace from git
 update_response=$(curl -X POST \
