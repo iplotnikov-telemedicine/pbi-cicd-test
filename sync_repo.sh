fcfb250b9de05c0e7f9dd2c0224f5f79ae72633c
Igor <plotnikov.sseu@gmail.com>
2024-03-07 10:34:17 -0500

    Separate sync_repo and update_workspace

---
+++ sync_repo.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+AZORG=$AZORG
+AZPROJECT=$AZPROJECT
+AZUREPAT=$AZUREPAT
+AZUSERNAME=$AZUSERNAME
+AZUSERPASSWORD=$AZUSERPASSWORD
+AZUSER_EMAIL=$AZUSER_EMAIL
+CLIENT_ID=$CLIENT_ID
+TENANT_ID=$TENANT_ID
+WORKSPACE_ID=$WORKSPACE_ID
+AZREPO="https://$AZUSERNAME:$AZUREPAT@dev.azure.com/$AZORG/$AZPROJECT/_git/pbi-cicd-test"
+
+
+# Remove Git information (for fresh git start)
+rm -rf Brain-Squeezes/.git
+
+# Fetch the changes from Azure DevOps to ensure we have latest
+git fetch --unshallow
+
+# Pull changes from Azure DevOps if its exiting branch and have commits on it
+git pull $AZREPO
+
+# Set Git user identity
+git config --global user.email "$AZUSER_EMAIL"
+git config --global user.name "$AZUSERNAME"
+
+# Add all changes into stage, commit, and push to Azure DevOps
+git add .
+git commit -m "Sync from GitHub to Azure DevOps"
+git push --force $AZREPO
--- update_workspace.sh
+++ update_workspace.sh
@@ -1,51 +1,20 @@
-#!/bin/bash
-
-AZUREPAT=$AZUREPAT
-AZUSERNAME=$AZUSERNAME
-AZUSERPASSWORD=$AZUSERPASSWORD
-AZUSER_EMAIL=$AZUSER_EMAIL
-AZORG=$AZORG
-AZPROJECT=$AZPROJECT
-AZREPO="https://$AZUSERNAME:$AZUREPAT@dev.azure.com/$AZORG/$AZPROJECT/_git/pbi-cicd-test"
-CLIENT_ID=$CLIENT_ID
-TENANT_ID=$TENANT_ID
-
-# Remove Git information (for fresh git start)
-rm -rf Brain-Squeezes/.git
-
-# Fetch the changes from Azure DevOps to ensure we have latest
-git fetch --unshallow
-
-# Pull changes from Azure DevOps if its exiting branch and have commits on it
-git pull $AZREPO
-
-# Set Git user identity
-git config --global user.email "$AZUSER_EMAIL"
-git config --global user.name "$AZUSERNAME"
-
-# Add all changes into stage, commit, and push to Azure DevOps
-git add .
-git commit -m "Sync from GitHub to Azure DevOps"
-git push --force $AZREPO
-
-
 # Get access token
 token_response=$(curl --location "https://login.windows.net/common/oauth2/token" \
     --header "Content-Type: application/x-www-form-urlencoded" \
     --header "Cookie: fpc=AqaCGRSl6mZAgM-xxtlFBrbzHxbfAQAAADqaet0OAAAAYDQJkgEAAABEmnrdDgAAAA; stsservicecookie=estsfd; x-ms-gateway-slice=estsfd" \
-    --data-urlencode "client_id=c66a018d-5fee-460a-952d-06a9aada27bc" \
+    --data-urlencode "client_id=$CLIENT_ID" \
     --data-urlencode "grant_type=password" \
     --data-urlencode "resource=https://analysis.windows.net/powerbi/api" \
     --data-urlencode "username=igor.plotnikov@credentially.io" \
     --data-urlencode "password=$AZUSERPASSWORD" \
-    --data-urlencode "tenant_id=829ad7f2-b91c-4435-9cb3-364001840d36")
+    --data-urlencode "tenant_id=$TENANT_ID")
 
 access_token=$(echo "$token_response" | grep -o '"access_token":"[^"]*' | awk -F'"' '{print $4}')
 
 # Get status of the workspace
 status_response=$(curl -s -X GET \
   -H "Authorization: Bearer $access_token" \
-  https://api.fabric.microsoft.com/v1/workspaces/99693b73-c010-4b60-b5bf-11c970b05b09/git/status)
+  https://api.fabric.microsoft.com/v1/workspaces/$WORKSPACE_ID/git/status)
 
 # Extract parameters using Bash string manipulation
 workspaceHead=$(echo "$status_response" | grep -o '"workspaceHead":"[^"]*' | sed 's/"workspaceHead":"//')
@@ -75,7 +44,7 @@ update_response=$(curl -X POST \
           "allowOverrideItems": true
         }
       }' \
-  https://api.fabric.microsoft.com/v1/workspaces/99693b73-c010-4b60-b5bf-11c970b05b09/git/updateFromGit)
+  https://api.fabric.microsoft.com/v1/workspaces/$WORKSPACE_ID/git/updateFromGit)
 
 if [ "$update_response" = "null" ]; then
   echo "Success"
